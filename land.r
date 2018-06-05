053580a8a706ca7105f6ccacc101ac43b8dfb1d3
Daniel Bion Barreiros <bion@manifestohut.com>
2018-06-04 23:26:19 -0300

    - Equação 12

--- land.r
+++ land.r
@@ -8,11 +8,12 @@ cov = cov(x) # Matriz de covariância estimada
 t = 0
 
 S = dim(x)[1] # Numero de observações
+N = S
 D = dim(x)[2] # Numero of atributos
 Z = sqrt(((2*pi)^D) * det(cov)) # Constante de normalização da distribuição normal Euclidiana
 
 
-# Equação 7
+# Equação 7: Estimar M para estimar C
 sigma = 0.5
 w = matrix(0, S, S)
 for(i in 1:S){
@@ -23,17 +24,17 @@ for(i in 1:S){
 
 p = 0.01
 M = list()
-for(i in 1:S){
+for(s in 1:S){
     tempM = matrix(0, D, D)        
     for(d in 1:D){
-        for(j in 1:S){        
-            tempM[d, d] = tempM[d, d] + (w[i, j] * ((x[i,d] - x[j,d])^2) + p)
+        for(n in 1:N){        
+            tempM[d, d] = tempM[d, d] + (w[s, n] * ((x[s,d] - x[n,d])^2) + p)
         }
     }
     M[[i]] = solve(tempM)
 }
 
-# Equação 10
+# Equação 10: Estimar C
 v = mvrnorm(S, rep(0, D), cov) # Normal com média 0 e matriz de covariância estimada
 
 summ = 0
@@ -41,6 +42,19 @@ for(s in 1:S){
     summ = summ + m(mu, v[s], M)
 }
 C = Z / S * summ
+
+# Equação 12: Estimar d para ajustar a média
+summ = 0
+for(n in 1:N){    
+    for(s in 1:S){
+        summ = summ + Log(mu, x[n]) - ((Z / (C * S)) * m(mu, v[s], M) * v[s])
+    }
+}
+delta = solve(-cov) %*% (summ/N)
+d = -cov %*% delta
+
+# Quem é alfa?
+mu = Exp(alfa_mu * d)
 
 m = function(mu, v, M){
     det(M %*% Exp(mu, v))^0.5
